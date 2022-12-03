48f305d5df803724df3d10b729734b3b0eb28627
MichaelSv <michaelsvistunoff@gmail.com>
2022-12-04 05:19:49 +0600

    Корректировка кода: оптимизация

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
-﻿Console.WriteLine("Enter the size of Array:");
+﻿Console.Write("Enter the size of Array: ");
 int arrSize = int.Parse(Console.ReadLine()!);
+
+string[] originArr = Array(arrSize);
+string[] finalArr = newArray(originArr, LengthOfTheNewArray(originArr));
+Console.WriteLine($"Here is your arrays: [{String.Join(", ", originArr)}] -> [{String.Join(", ", finalArr)}]");
 
 string[] Array (int size) {
     string[] tempArr = new string[size];
     for (int i=0; i < tempArr.Length; i++) {
-        Console.WriteLine($"Input the {i}-row");
+        Console.Write($"Input the {i}-row: ");
         tempArr[i] = Console.ReadLine()!;
     }
     return tempArr;
 }
-
-string[] originArr = Array(arrSize);
-Console.WriteLine($"You've inputed next array: [{String.Join(", ", originArr)}]");
 
 int LengthOfTheNewArray (string[] gotedArr) {
     int getTheSizeOfTheNewArr = 0;
@@ -36,6 +37,3 @@ string[] newArray (string[] gotedArr, int size) {
     }
     return tempArr;
 }
-
-string[] finalArr = newArray(originArr, LengthOfTheNewArray(originArr));
-Console.WriteLine($"Your final array: [{String.Join(", ", finalArr)}]");
