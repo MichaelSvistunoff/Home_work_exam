82b3272d741de8bd75242644fb0bf43132e62ca1
MichaelSv <michaelsvistunoff@gmail.com>
2022-12-04 05:07:54 +0600

    Метод ввда массива с клавиатуры

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter the size of Array:");
+int arrSize = int.Parse(Console.ReadLine()!);
+
+string[] Array (int size) {
+    string[] tempArr = new string[size];
+    for (int i=0; i < tempArr.Length; i++) {
+        Console.WriteLine($"Input the {i}-row");
+        tempArr[i] = Console.ReadLine()!;
+    }
+    return tempArr;
+}
+
+string[] originArr = Array(arrSize);
+Console.WriteLine($"You've inputed next array: [{String.Join(", ", originArr)}]");
